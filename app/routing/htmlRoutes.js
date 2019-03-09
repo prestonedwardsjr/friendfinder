9d7fea2eca8929857bffda1123118fd6dfdf92c3
fullcourtpres <pres@imdiversity.com>
2019-03-08 22:01:03 -0600

    my 1st commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var router = require("express").Router();
+var path = require("path");
+
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../public/home.html"));
+});
+
+router.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../public/survey.html"));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var htmlRoutes = require("./app/routing/htmlRoutes.js")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+// var path;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.use(htmlRoutes);
+
+app.listen(PORT,function(err){
+    if (err){
+      console.log(err);
+    }
+    console.log("connected to Port "  + PORT)
+});
